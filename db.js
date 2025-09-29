839d02894d1ef263b40ca49efe68316a3f3994c5
Ummu-Rumaan-M <ummurumaan06@gmail.com>
2025-09-29 14:36:12 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,15 @@
+// config/db.js
+const mongoose = require('mongoose');
+
+async function connectDB() {
+  const uri = process.env.MONGO_URI;
+  if (!uri) throw new Error('MONGO_URI not defined in .env');
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log('✅ MongoDB connected');
+}
+
+module.exports = connectDB;
